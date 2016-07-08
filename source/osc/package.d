a89af7f5ec4a655c8b07442de3310c5d8f4e4a87
tanitta <ttata.trit@gmail.com>
2016-07-08 19:34:55 +0900

    Add addressSpace.

--- source/osc/package.d
+++ source/osc/package.d
@@ -8,6 +8,7 @@ struct Packet {
     }//public
 
     private{
+        int _size;
         Message _message;
         Bundle _bundle;
     }//private
@@ -20,7 +21,7 @@ struct Bundle {
     }//public
 
     private{
-        immutable string _prefix = "#bundle";
+        OscString!('\0') _header = OscString("#bundle");
         // _timeTag
         //first size
         //first content
@@ -37,7 +38,7 @@ struct BundleElement {
     }//public
 
     private{
-        //size
+        int _size;
         //content 
     }//private
 }//struct BundleElement
@@ -49,8 +50,8 @@ struct Message {
     }//public
 
     private{
-        // address pattern
-        // type tag string
+        AddressPattern _addressPattern;
+        TypeTagString _typeTagString;
         // args
     }//private
 }//struct Message
@@ -82,5 +83,74 @@ class Server {
     }//public
 
     private{
+        AddressSpace _addressSpace;
     }//private
 }//class Server
+
+/++
++/
+class AddressSpace {
+    public{
+    }//public
+
+    private{
+        Container _container;
+    }//private
+}//class AddressSpace
+
+// TODO
+bool isMatch(in AddressPattern addressPattern, const AddressSpace addressSpace){
+    return false;
+}
+
+// bool isMatchRec(in Container container, in AddressSpace addressSpace){
+// }
+
+/++
++/
+struct Container {
+    public{
+    }//public
+
+    private{
+        Container[string] _containers;
+        Method[string] _methods;
+        
+            
+    }//private
+}//struct Container
+
+bool hasMethod(in Container container, in string methodName){
+    import std.algorithm;
+    return container._methods.keys.canFind(methodName);
+}
+
+unittest{
+    auto container = Container();
+    assert(!container.hasMethod("hoge"));
+    container._methods["hoge"] = Method();
+    assert(container.hasMethod("hoge"));
+}
+
+bool hasContainer(in Container container, in string containerName){
+    import std.algorithm;
+    return container._containers.keys.canFind(containerName);
+}
+
+unittest{
+    auto container = Container();
+    assert(!container.hasContainer("hoge"));
+    container._containers["hoge"] = Container();
+    assert(container.hasContainer("hoge"));
+}
+
+/++
++/
+struct Method {
+    public{
+    }//public
+
+    private{
+    }//private
+}//struct Method
+
